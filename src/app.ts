3fa67272baa6e4ffbab1e73074f11d0ef4facda2
MD AMINUL ISLAM <mr.aminul118@gmail.com>
2025-07-08 19:18:11 +0600

    User route setup and make user create api

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,28 @@
 import express, { Request, Response } from "express";
+import router from "./app/routes";
+import cors, { CorsOptions } from "cors";
 const app = express();
 
+const whitelist = ["http://example1.com", "http://example2.com"];
+
+const corsOptions: CorsOptions = {
+  origin: (origin, callback) => {
+    if (!origin || whitelist.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+};
+
+// Middlewares
+app.use(express.json());
+app.use(cors(corsOptions));
+
+// Api routing
+app.use("/api/v1", router);
+
+// Testing api
 app.get("/", (req: Request, res: Response) => {
   res.status(200).json({
     message: "Tour Management server running",
---
+++ src/app/modules/user/user.controller.ts
@@ -0,0 +1,24 @@
+import { Request, Response } from "express";
+import httpStatus from "http-status-codes";
+import { User } from "./user.model";
+
+const createUser = async (req: Request, res: Response) => {
+  try {
+    const { name, email } = req.body;
+    const user = await User.create({ name, email });
+    res.status(httpStatus.CREATED).json({
+      message: "User created Successfully",
+      user,
+    });
+  } catch (error) {
+    // console.log(error);
+    res.status(httpStatus.BAD_REQUEST).json({
+      message: "Something Went Wrong",
+      error,
+    });
+  }
+};
+
+export const UserControllers = {
+  createUser,
+};
