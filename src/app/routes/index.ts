94106b7b6ec5c7e33982586419c7772bd97dc38b
MD AMINUL ISLAM <mr.aminul118@gmail.com>
2025-07-17 23:34:03 +0600

    Division crud operation

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import { UserRoutes } from "../modules/user/user.routes";
 import AuthRouter from "../modules/auth/auth.route";
+import divisionRouter from "../modules/division/division.route";
 
 const router = Router();
 
@@ -12,6 +13,10 @@ const moduleRoutes = [
   {
     path: "/auth",
     route: AuthRouter,
+  },
+  {
+    path: "/division",
+    route: divisionRouter,
   },
 ];
 
